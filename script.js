a8f42691130f453750be3edbc525beb67dccccaf
Ramneet Kang <ramneetkang2000@gamil.com>
2026-03-22 17:03:09 +0530

    Add function for clear button and extend operations to multi-digit numbers

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let multiply = function (a, b) {
 }
 
 let divide = function (a, b) {
-    if (b == 0){
+    if (b == 0) {
         return "Error! Cannot divide by zero."
     }
     return a / b;
@@ -38,35 +38,53 @@ let operate = function (a, operator, b) {
     return result;
 }
 
-let num1, num2, operator;
+let num1 = null, num2 = null, operator = null;
 
 let display = document.querySelector("input");
 let btn = document.querySelectorAll("button");
 
-btn.forEach(function(btn) {
+btn.forEach(function (btn) {
     btn.addEventListener("click", function (e) {
 
         let value = btn.value;
 
         if (btn.classList.contains("number")) {
-            if (num1 === undefined){
-                num1 = Number(value);
-                display.value += num1;
+
+            if (operator === null) {
+                if (num1 === null) {
+                    num1 = value;
+                }
+                else {
+                    num1 += value;
+                }
             }
 
-            else if (num2 === undefined){
-                num2 = Number(value);
-                display.value += num2;
+            else {
+                if (num2 === null) {
+                    num2 = value;
+                }
+                else {
+                    num2 += value;
+                }
             }
+
+            display.value  += value;
         }
 
-        else if (btn.classList.contains("operator")){
+        else if (btn.classList.contains("operator")) {
             operator = value;
             display.value += operator;
         }
 
-        else if (value === "="){
-            display.value = operate(num1, operator, num2);
+        else if (value === "=") {
+            display.value = operate(Number(num1), operator, Number(num2));
+        }
+
+        else if (value === "clear") {
+            display.value = "";
+            num1 = null;
+            num2 = null;
+            operator = null;
         }
     })
 })
